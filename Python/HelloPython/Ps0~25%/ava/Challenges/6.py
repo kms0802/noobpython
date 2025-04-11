eacc720acbc518963a9179b2efd9dbbd50127aba
kms0802 <kms0802@cju.ac.kr>
2025-04-11 18:21:44 +0900

    파이썬 문자열7

---
+++ Python/HelloPython/Ps0~25%/ava/Challenges/6.py
@@ -0,0 +1,26 @@
+text = "I love Python"
+print(len(text))
+print(text[0])
+print(text[4])
+print(text[-1])
+text3 = "HackerThePlanet"
+print(text3[0:4])
+print(text[7:])
+
+msg = "i aM a hACKer"
+
+print(msg.lower())
+print(msg.upper())
+print(msg.capitalize())
+
+text4 = "admin123"
+next_text = text4.replace("admin", "user")
+print(next_text)
+text5 = "Exploit detected"
+print("ploit" in text)
+
+data = "id:root.password:1234.email:hack@web.com"
+print(data.split(':'))
+
+text5 = "   exploit   "
+print(text.strip())
