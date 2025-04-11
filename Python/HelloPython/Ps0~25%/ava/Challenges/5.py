5e85180e2ca1f9a49b87d59a75acb874b585fe87
kms0802 <kms0802@cju.ac.kr>
2025-04-11 15:45:50 +0900

    문자열

---
+++ Python/HelloPython/Ps0~25%/ava/Challenges/5.py
@@ -0,0 +1,22 @@
+text = "Hello, Hacker!"
+print(len(text))
+print(text[0])
+print(text[-1])
+print(text[0:5])
+print(text[7:])
+
+msg = "python is fun"
+
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+print(msg.replace("fun", "powerful"))
+
+print("hack" in text)
+print("Hacker" in text)
+
+data = "id.passwd.email"
+print(data.split(","))
+
+text = "   hello   "
+print(text.strip())
