b831b3dfa3cd154bb18ab9de31037e218a51c802
kms0802 <kms0802@cju.ac.kr>
2025-04-11 15:45:26 +0900

    변수 자료형

---
+++ Python/HelloPython/Ps0~25%/ava/Challenges/3.py
@@ -0,0 +1,4 @@
+name = "Alice"
+hackers_name = "Shadow"
+
+print("내 이름은" + name + "이고, 해커 네임은 " + hackers_name + "입니다.")
---
+++ Python/HelloPython/Ps0~25%/ava/Challenges/4.py
@@ -0,0 +1,9 @@
+a = 123
+b = "hello"
+c = 3.14
+d = False
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
