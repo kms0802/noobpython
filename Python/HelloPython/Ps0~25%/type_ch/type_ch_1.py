b2bfe26b498069608744266cef66e4bcb19f0858
kms0802 <kms0802@cju.ac.kr>
2025-04-11 19:17:33 +0900

    예제1

--- Python/HelloPython/Ps0~25%/type_ch/type_ch_1.py
+++ Python/HelloPython/Ps0~25%/type_ch/type_ch_1.py
@@ -1,3 +1,18 @@
 num_str = "42"
 num = int(num_str)
 print(num + 10)
+
+
+age = 10
+print("나는 " + str(age) + "살입니다.")
+
+pi_str = "3.14"
+pi = float(pi_str)
+print(pi * 2)
+
+x = input("숫자를 입력하세요: ")
+x = int(x)
+print(x + 5)
+
+print(type("123"))
+print(type(int("123")))
