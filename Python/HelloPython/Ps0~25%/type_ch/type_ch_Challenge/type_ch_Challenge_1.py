42736855278a7c6ea3b4c4323e48f3354ec8e417
kms0802 <kms0802@cju.ac.kr>
2025-04-11 19:32:32 +0900

    타입 변환 문제풀이

---
+++ Python/HelloPython/Ps0~25%/type_ch/type_ch_Challenge/type_ch_Challenge_1.py
@@ -0,0 +1,28 @@
+num = "25"
+num = int(num)
+print(num)
+
+age = 19
+age = str(age)
+print("당신의 나이는 " + age + "살입니다.")
+
+x = input("숫자1:")
+y = input("숫자2:")
+x = int(x)
+y = int(y)
+print(x + y)
+
+pi = "3.1415"
+pi = float(pi)
+print(pi * 2)
+
+a = "123"
+b = int(a)
+c = float(a)
+print(type(a))
+print(type(b))
+print(type(c))
+
+num = 7
+num = str(num)
+print("비밀번호는 " + num + "자리입니다.")
