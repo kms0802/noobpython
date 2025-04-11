018b22f36f3a7bc7958dbb8c40b153d984bf7b5f
kms0802 <kms0802@cju.ac.kr>
2025-04-11 18:30:56 +0900

    문자열7

---
+++ Python/HelloPython/Ps0~25%/ava/Challenges/7.py
@@ -0,0 +1,24 @@
+text = "Stay anoymous!"
+print(len(text))
+
+text1 = "ZeroDayExploit"
+print(text1[0])
+print(text1[5])
+print(text1[13])
+
+text2 = "hELLO wORlD"
+print(text2.lower())
+print(text2.upper())
+print(text2.capitalize())
+
+text3 = "root_access"
+print(text3.replace("root","user"))
+
+text4 = "Unauthorized access detected"
+print("access" in text4)
+
+text5 = "host:localhost.port:8080,protocol:http"
+print(text5.split(','))
+
+text6 = "   shellcode   "
+print(text6.strip())
