dd7c358876d8cc92afac5bc70ea6093bbe97bbd7
kms0802 <kms0802@cju.ac.kr>
2025-04-11 19:32:57 +0900

    복습

---
+++ Python/HelloPython/Ps0~25%/Final_Challenge/Final_Challenge_1.py
@@ -0,0 +1,12 @@
+name = input("이름:")
+age = input("나이:")
+locate = input("사는 곳:")
+
+print("안녕하세요 저는 " + name + "이고 " + age + "이며 " + locate + "에 삽니다.")
+
+x = int(input("x값:"))
+y = int(input("y값:"))
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
