1f3c84b4b8809d8cd00fdc04c9a444ee591ba81a
kms0802 <kms0802@cju.ac.kr>
2025-04-12 22:04:53 +0900

    리스트 내포 문제풀이 수정

--- Python/HelloPython/Ps50~75%/List_Comperhension/List_Comperhension_Challenges/List_Comperhension_Challenge_1.py
+++ Python/HelloPython/Ps50~75%/List_Comperhension/List_Comperhension_Challenges/List_Comperhension_Challenge_1.py
@@ -1,19 +1,21 @@
 print("1. Squares of even numbers(1-10)")
-squares = [x**2 for x in range(1, 10)]
+squares = [x**2 for x in range(1, 11) if x % 2 == 0]
 print(squares)
 print()
 
 print("2. Filter short words")
 words = ["root", "admin", "user", "guest", "test"]
-words_for = [len(word) for word in words if len(word) >= 4]
+words_for = [word for word in words if len(word) <= 4]
 print(words_for)
 
 print("3. Numbers divisible by 3 (1-30)")
-nums = [x for x in range(1, 30)]
+nums = [x for x in range(1, 31) if x % 3 == 0]
+print(nums)
 
 print("4. First letters of words")
 names = ["Neo", "Trinity", "Morpheus", "Smith"]
-#name_text = [name for name in names name.startswith("")]
+names_text = [name[0] for name in names]
+print(names_text)
 
 print("5. 192.168 대역 IP만 필터링")
 ips = ["192.168.0.1", "10.0.0.1", "192.168.1.1", "8.8.8.8"]
@@ -22,4 +24,5 @@ print(ips_filter)
 
 print("6. 빈 문자열 제외하고 길이만 출력")
 texts = ["hello", "", "world", "", "python"]
-texts_length = [text for text in texts]
+texts_length = [text for text in texts if text != ""]
+print(texts_length)
