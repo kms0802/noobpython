42b2ca35f681ebe202617b8d513b36bef301e056
kms0802 <kms0802@cju.ac.kr>
2025-04-12 13:10:43 +0900

    딕셔너리 문제풀이1 수정

--- Python/HelloPython/Ps50~75%/Dictionary/Dictionary_Challenges/Dictionary_Challenge_1.py
+++ Python/HelloPython/Ps50~75%/Dictionary/Dictionary_Challenges/Dictionary_Challenge_1.py
@@ -4,17 +4,20 @@ user = {
     "password" : "1234",
     "level" : "root"
 }
+print(user)
 
 #2. 값 접근하기
-print(user["id"])
-print(user["level"])
+print("ID", user["id"])
+print("Level", user["level"])
 
 #3. 값 수정과 추가
 user["password"] = "5678"
-#user.add("email", "admin@web.com")
+user["email"] = "admin@web.com"
+print(user)
 
 #4. 값 삭제하기
-#user.remove("password")
+del user["password"]
+print(user)
 
 #5. 반복 출력하기
 for key in user:
@@ -27,8 +30,9 @@ users = {
     "neo": "marix"
 }
 
-id = input("admin")
-pw = input("1234")
+user_input = input("아이디를 입력하세요: ")
 
-for uss in users:
-    if id == "admin"
+if user_input in users:
+    print("접속 허용")
+else:
+    print("접속 거부")
